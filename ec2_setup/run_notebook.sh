e3bbfdc99e9211666a81e2c91915f553b7bf5e68
Jonathan Helmus <jjhelmus@gmail.com>
2017-01-20 11:22:46 -0600

    Add files for Docker setup

---
+++ ec2_setup/run_notebook.sh
@@ -0,0 +1,2 @@
+#/root/miniconda3/envs/ams-workshop/bin/jupyter notebook --certfile=~/certs/mycert.pem --keyfile ~/certs/mycert.key
+/root/miniconda3/envs/ams-workshop/bin/jupyter notebook
---
+++ ec2_setup/run_setup.ec2.sh
@@ -0,0 +1,4 @@
+cp /io/setup_ec2.sh ~
+cp /io/environment.yml ~
+cd ~
+./setup_ec2.sh
